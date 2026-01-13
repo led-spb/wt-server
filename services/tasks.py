59918619827a5f5fbf7616db38662a0811c01bb1
Aleksey Ponimash <aleksey.ponimash@rt.ru>
2026-01-13 15:59:03 +0300

    task service repeat words

--- services/tasks.py
+++ services/tasks.py
@@ -10,11 +10,29 @@ class TaskService:
 
     @classmethod
     def get_user_spelling_task(cls, user :User, count: int = 20, min_level :int = 0, max_level :int = 10) -> List[Word]:
-        query = db.select(
-            Word
-        ).options(
-            joinedload(Word.spellings)
-        ).outerjoin(
+        words = []
+        repeat_count = count // 5
+
+        repeat_query = db.select(Word).options(joinedload(Word.spellings)).join(
+            WordStatistics,
+            and_(
+                Word.id == WordStatistics.word_id,
+                WordStatistics.user_id == user.id,
+                WordStatistics.failed > 0
+            )
+        ).filter(
+            Word.spellings.any()
+        ).filter(
+            Word.level >= min_level
+        ).filter(
+            Word.level <= max_level
+        ).order_by(
+            func.random()
+        ).limit(repeat_count)
+
+        words += db.session.execute(repeat_query).unique().scalars().all()
+
+        query = db.select(Word).options(joinedload(Word.spellings)).outerjoin(
             WordStatistics,
             and_(
                 Word.id == WordStatistics.word_id,
@@ -26,9 +44,13 @@ class TaskService:
             Word.level >= min_level
         ).filter(
             Word.level <= max_level
+        ).filter(
+            Word.id.notin_([word.id for word in words])
         ).order_by(
             nulls_first(WordStatistics.failed+WordStatistics.success), func.random()
         ).limit(
-            count
+            count - len(words)
         )
-        return db.session.execute(query).unique().scalars()
+
+        words += db.session.execute(query).unique().scalars()
+        return words
